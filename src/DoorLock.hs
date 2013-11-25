bb2f56d68452a4344d608e0f1ade950b26bc7573
llllllllll <joejev@gmail.com>
2013-11-25 14:38:43 -0500

    added config files, and no more passowords in text.

--- src/DoorLock.hs
+++ src/DoorLock.hs
@@ -5,18 +5,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module DoorLock
-    ( lock_door         -- :: IO ()
+    ( module System.Hardware.Arduino
+    , lock_door         -- :: IO ()
     , unlock_door       -- :: IO ()
-    , query_lock_status -- :: IO ()
-    , send_response     -- :: String -> IO ()
     ) where
 
-import Control.Concurrent             (forkIO)
-import Control.Monad                  (when,liftM)
-import Control.Monad.Trans            (liftIO)
-import qualified Data.Text.Lazy as TL (pack)
-import Network.Mail.SMTP              (Address(..),sendMailWithLogin
-                                      ,simpleMail,plainTextPart)
+import Control.Concurrent      (forkIO)
+import Control.Monad           (when,liftM)
+import Control.Monad.Trans     (liftIO)
 import System.Hardware.Arduino
 
 -- Lock the door.
@@ -50,48 +46,8 @@ unlock_door = do
               pot = analog 3
           setPinMode pot ANALOG
           val <- analogRead pot
-          liftIO $ print val
           setPinMode led OUTPUT
           digitalWrite led True
           delay 1000
           digitalWrite led False
           delay 1000
-
--- Query the arduino to check if the door is locked or not and then send an
--- appropriate response
--- A value greater than to 512 is LOCKED
--- a value less than or equal to 512 is UNLOCKED
-query_lock_status :: IO ()
-query_lock_status = liftIO (putStrLn "querying door")
-                    >> (withArduino False "/dev/ttyACM0" $ do
-                            setPinMode (analog 1) ANALOG
-                            n <- analogRead (analog 1)
-                            v <- timeOut 10000 (read_val n)
-                            let val = case v of
-                                          Nothing -> 0
-                                          Just a  -> a
-                            liftIO $ print val
-                            when (val > 512)  $ liftIO
-                                     $ send_response "door is locked"
-                            when (val <= 512) $ liftIO
-                                     $ send_response "door is unlocked")
-  where
-      read_val v = do
-          n <- analogRead (analog 1)
-          if n == v
-            then read_val n
-            else return n
-
--- Sends a message response to my phone.
-send_response :: String -> IO ()
-send_response msg = sendMailWithLogin "email.wit.edu" username password mail
-  where
-      mail         = simpleMail from to cc bcc subject [body]
-      from         = Address Nothing "jevnikj@wit.edu"
-      to           = [Address (Just "Joe Jevnik")  "8602024741@txt.att.net"]
-      cc           = []
-      bcc          = []
-      subject      = ""
-      body         = plainTextPart $ TL.pack msg
-      username     = "jevnikj@wit.edu"
-      password     = "my_pass_true_txt_;_;" -- Deal with this!
--- src/Main.hs
+++ src/Main.hs
@@ -2,33 +2,171 @@
 -- 24.11.2013
 -- polls the server for new messages and acts accordingly.
 
-{-# LANGUAGE OverloadedStrings #-}
-
 import Control.Applicative            ((<$>))
-import Control.Concurrent             (threadDelay)
-import Control.Monad                  (when,unless)
-import System.Directory               (getDirectoryContents,removeFile)
+import Control.Concurrent             (forkIO,threadDelay)
+import Control.Monad                  (when,unless,void)
+import Control.Monad.Trans            (liftIO)
+import Data.List                      (nub,stripPrefix)
+import Data.Maybe                     (fromMaybe,isJust)
+import qualified Data.Text as T       (pack)
+import qualified Data.Text.Lazy as TL (pack)
+import Network.Mail.SMTP              (Address(..),sendMailWithLogin
+                                      ,simpleMail,plainTextPart)
+import System.Directory               (getDirectoryContents
+                                      ,removeFile,getHomeDirectory
+                                      ,createDirectoryIfMissing)
+import System.Environment             (getArgs)
+import System.FilePath                ((</>))
+import System.IO                      (hSetEcho,hFlush,stdin,stdout)
+import System.Process                 (system)
 import Text.Regex.TDFA                ((=~))
 
+-- WARNING: ARDUINO SPECIFIC: PROTOTYPE ONLY
 import DoorLock
 
--- The directory where new messages will appear.
-mAIL_DIR :: FilePath
-mAIL_DIR = "/home/joejev/maildir/INBOX/new/"
 
--- Polls every minute to see if any commands need to be dealt with.
-poll :: IO ()
-poll = do
-    msg_fls <- map (mAIL_DIR ++) . filter (`notElem` [".",".."])
-               <$> getDirectoryContents mAIL_DIR
+-- -----------------------------------------------------------------------------
+-- data and names
+
+-- A data type to hold all the server's settings during runtime.
+data Settings = Settings { smtp_server  :: String
+                         , sender_name  :: String
+                         , sender_email :: String
+                         , sender_pass  :: String
+                         , lock_pass    :: String
+                         , mail_dir     :: FilePath
+                         } deriving Show
+
+-- default location of the config file.
+config_fl_def :: IO FilePath
+config_fl_def = getHomeDirectory >>= \s -> return (s ++ "/.door_lock/config")
+
+-- -----------------------------------------------------------------------------
+-- parsing and initalization
+
+-- Parses a settings file for it's data.
+parse_settings :: String -> String -> String -> Settings
+parse_settings str server_pass door_pass =
+    let ls     = filter (\l -> (not . null) l && head l /= '#') $ lines str
+        server = parse_ln "server="   ls
+        name   = parse_ln "name="     ls
+        email  = parse_ln "email="    ls
+        mail   = parse_ln "mail_dir=" ls
+    in Settings { smtp_server  = server
+                , sender_name  = name
+                , sender_email = email
+                , sender_pass  = server_pass
+                , lock_pass    = door_pass
+                , mail_dir     = mail
+                }
+  where
+      parse_ln str ls = dropWhile (== ' ') $ fromMaybe ""
+                        $ head $ filter isJust $ map (stripPrefix str) ls
+
+-- Returns the sender of the request, the recipient of the response.
+parse_rcpt :: String -> String
+parse_rcpt str = (tail . init) $ dropWhile (/= '<') $ str =~ "Return-Path: <.*>"
+
+-- Parses a request and acts accordingly.
+handle_req :: Settings -> String -> IO ()
+handle_req s req  = do
+    if req =~ lock_pass s
+      then do
+          when (req =~ "lock=true") $
+               lock_door >> send_response s (parse_rcpt req) "door locked"
+          when (req =~ "lock=false") $
+               unlock_door >> send_response s (parse_rcpt req) "door unlocked"
+          when (req =~ "lock_stat") $
+               query_lock_respond s (parse_rcpt req)
+      else
+          send_response s (parse_rcpt req) "incorrect password"
+
+-- -----------------------------------------------------------------------------
+-- main
+main :: IO ()
+main = do
+    args <- getArgs
+    let args' = filter (/= "-s") args
+    putStr "Door Password: "
+    hFlush stdout
+    hSetEcho stdin False
+    door_pass <- getLine
+    putStr "\nMail Password: "
+    server_pass <- getLine
+    hSetEcho stdin True
+    if any (== "-s") args
+      then void $ forkIO $ void $ system "offlineimap"
+      else return ()
+    putStrLn "\nPolling..."
+    if null args'
+      then do
+          config <- config_fl_def >>= readFile
+          let s = parse_settings config server_pass door_pass
+          createDirectoryIfMissing True (mail_dir s)
+          poll s
+      else do
+          config <- readFile $ head args'
+          let s = parse_settings config server_pass door_pass
+          createDirectoryIfMissing True (mail_dir s)
+          poll s
+
+-- Polls every second to check for new messages to deal with them.
+-- Deletes the messages that it has consumed, even if they are not valid.
+poll :: Settings -> IO ()
+poll s = do
+    msg_fls <- map (mail_dir s </>) . filter (`notElem` [".",".."])
+               <$> getDirectoryContents (mail_dir s)
     unless (null msg_fls) $ do
-                msgs <- mapM readFile msg_fls
-                let lock_true  = any (=~ ("lock=true" :: String))  msgs
-                    lock_false = any (=~ ("lock=false" :: String)) msgs
-                    lock_stat  = any (=~ ("lock_stat" :: String))  msgs
-                when lock_true (lock_door >> send_response "door locked!")
-                when lock_false (unlock_door >> send_response "door unlocked!")
-                when lock_stat query_lock_status
+                reqs <- mapM readFile msg_fls
+                mapM_ (handle_req s) reqs
                 mapM_ removeFile msg_fls
-    threadDelay 500000
-    poll
+    threadDelay 1000000
+    poll s
+
+-- -----------------------------------------------------------------------------
+-- mail handling.
+-- Polls every minute to see if any commands need to be dealt with.
+
+-- Sends a message response
+send_response :: Settings -> String -> String -> IO ()
+send_response s rcpt msg = sendMailWithLogin (smtp_server s)
+                           username password mail
+  where
+      mail         = simpleMail from to cc bcc subject [body]
+      from         = Address Nothing (T.pack (sender_email s))
+      to           = [Address Nothing (T.pack rcpt)]
+      cc           = []
+      bcc          = []
+      subject      = T.pack ""
+      body         = plainTextPart $ TL.pack msg
+      username     = sender_name s
+      password     = sender_pass s
+
+-- -----------------------------------------------------------------------------
+-- arduino specific
+-- WARNING: PROTOTYPE ONLY
+
+-- Query the arduino to check if the door is locked or not and then send an
+-- appropriate response
+-- A value greater than to 512 is LOCKED
+-- a value less than or equal to 512 is UNLOCKED
+query_lock_respond :: Settings -> String -> IO ()
+query_lock_respond s rcpt =
+    liftIO (putStrLn "querying door")
+               >> (withArduino False "/dev/ttyACM0" $ do
+                       setPinMode (analog 1) ANALOG
+                       n <- analogRead (analog 1)
+                       v <- timeOut 10000 (read_val n)
+                       let val = case v of
+                                     Nothing -> 0
+                                     Just a  -> a
+                       when (val > 512)  $ liftIO
+                                $ send_response s rcpt "door is locked"
+                       when (val <= 512) $ liftIO
+                                $ send_response s rcpt "door is unlocked")
+  where
+      read_val v = do
+          n <- analogRead (analog 1)
+          if n == v
+            then read_val n
+            else return n
